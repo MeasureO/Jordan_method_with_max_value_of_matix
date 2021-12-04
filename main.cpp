0c8ecacf7c51f76ea654a66e95b965dc817617c1
MeasureO <fedotov.dmitry.sergeevich@gmail.com>
2021-12-04 19:18:20 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 #include "matrix.h"
 using namespace std;
 
-int main()
-{
-    return 0;
+
+int main(int argc, char** argv){
+
+  for(int i = 0; i < argc; i++)
+  {
+    cout << "Command line parameter:" << argv[i] << endl;
+  }
 }
