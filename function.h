140f37c122d42efa3a552334aaf395376facfa11
MeasureO <fedotov.dmitry.sergeevich@gmail.com>
2021-12-04 21:24:56 +0300

    fix

--- function.h
+++ function.h
@@ -1,3 +1,17 @@
+#include<algorithm>
+#include<cmath>
 Matrix func(Matrix matrix, int k, int n, int i, int j){
+    if (k == 1){
+        matrix[i * matrix._size + j] = n - std::max(i, j) + 1;
+    }
+    if (k == 2){
+        matrix[i * matrix._size + j] = std::max(i, j)
+    }
+    if (k == 3){
+        matrix[i * matrix._size + j] = std::abs(i - j)
+    }
+    if (k == 4){
+        matrix[i * matrix._size + j] = 1.0 / (i + j - 1)
+    }
     return matrix;
 }
