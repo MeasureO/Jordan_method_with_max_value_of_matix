da6c29eceb8a9c0e5711c17b7ffb88f86e165fdc
MeasureO <fedotov.dmitry.sergeevich@gmail.com>
2021-12-04 18:13:25 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -3,16 +3,6 @@
 #include "matrix.h"
 using namespace std;
 
-class Matrix
-{ 
-    private:
-        int _size;
-        vector<double> _data;
-    public:
-        Matrix() {
-        }
-};
-
 int main()
 {
     return 0;
