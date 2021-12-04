cdd83f25912c7d0d8167d524f0ac02e139389c36
MeasureO <fedotov.dmitry.sergeevich@gmail.com>
2021-12-04 18:34:54 +0300

    fix

--- matrix.h
+++ matrix.h
@@ -9,6 +9,17 @@ public:
     Matrix(int size) {
         _data.resize(size * size);
     }
-    
-
+    friend istream& operator>>(istream& is, const Matrix& mt);
+    friend ostream& operator<<(ostream& os, const Matrix& mt);
 };
+
+istream& operator>>(istream& is, const Matrix& mt)
+{
+    // is >> endl;
+    return os;
+}
+ostream& operator<<(ostream& os, const Matrix& mt)
+{
+    // os << dt.mo << '/' << dt.da << '/' << dt.yr;
+    return os;
+}
