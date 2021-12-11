ffb3fa83e283f7476314f2aca2af484daece3f75
measure0 <fedotov.dmitry.sergeevich@gmail.com>
2021-12-11 20:35:13 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -3,45 +3,60 @@
 #include <iostream>
 #include "matrix.h"
 #include "function.h"
+#include "jordan.h"
 using namespace std;
 
-
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     Matrix matrix;
-    if (atoi(argv[3]) == 0){
+    if (atoi(argv[3]) == 0)
+    {
         freopen("input.txt", "r", stdin); // перенаправить стандартный поток вывода в файл
         matrix = Matrix(atoi(argv[1]));
         std::cin >> matrix;
         fclose(stdin);
     }
-    if (atoi(argv[3]) != 0){
+    if (atoi(argv[3]) != 0)
+    {
         matrix = Matrix(atoi(argv[1]));
         int n = matrix.getSize();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 matrix(i, j) = func(atoi(argv[3]), n, i, j);
             }
-        }  
-        // for(int i = 0; i < matrix._size; i++){
-        //     for(int j = 0; j < matrix._size; j += 2){
-        //         matrix._b[i] += matrix._data[i * matrix._size + j];
-        // }
-        //}
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j += 2)
+            {
+                matrix._b[i] += matrix(i, j);
+            }
+        }
     }
+    std::vector<double> x;
+    x.resize(atoi(argv[1]));
     //freopen("CON","w",stdout);
     std::cout << matrix;
-    std::cout << "-----------------------" << endl;
-    matrix.colSwap(1, 2);
-    std::cout << "After columns 1 and 2 swap: " << endl;
-    std::cout << matrix;
-    std::cout << "-----------------------" << endl;
-    std::cout << "After rows 1 and 2 of right_bottom_submatrix swap: " << endl;
-    matrix.rowSwap(1, 2, 2);
-    std::cout << matrix;
-    std::cout << "-----------------------" << endl;
-    std::cout << "Submatrix max: " << matrix.maxElem(matrix.getSize()).first << " " <<  matrix.maxElem(matrix.getSize()).second << endl;
+    std::cout << "----------------------" << std::endl;
+    // std::cout << "-----------------------" << endl;
+    // matrix.colSwap(1, 2);
+    // std::cout << "After columns 1 and 2 swap: " << endl;
+    // std::cout << matrix;
+    // std::cout << "-----------------------" << endl;
+    // std::cout << "After rows 1 and 2 of right_bottom_submatrix swap: " << endl;
+    // matrix.rowSwap(1, 2, 2);
+    // std::cout << matrix;
+    // std::cout << "-----------------------" << endl;
+    // for (int i = 0; i < atoi(argv[1]); i++)
+    // {
+    //     std::cout << "Submatrix max: " << matrix.maxElem(matrix.getSize() - i).first << " " << matrix.maxElem(matrix.getSize() - i).second << endl;
+    // }
+    //std::cout << "Submatrix max: " << matrix.maxElem(matrix.getSize()).first << " " << matrix.maxElem(matrix.getSize()).second << endl;
     //std::cout << matrix.getSize();
-    for(int i = 0; i < argc; i++)
+    jordan_solver(atoi(argv[1]), matrix, matrix._b, x);
+    for (int i = 0; i < argc; i++)
     {
         cout << "Command line parameter:" << argv[i] << endl;
     }
--- matrix.h
+++ matrix.h
@@ -1,31 +1,37 @@
 #pragma once
-class Matrix {
+#include <iostream>
+class Matrix
+{
 private:
     int _size;
     std::vector<double> _data;
-    // std::vector<double> _b;
+
 public:
-    Matrix() {
+    std::vector<double> _b;
+    Matrix()
+    {
         _size = 0;
     }
 
-    Matrix(int size) {
+    Matrix(int size)
+    {
         _size = size;
         _data.resize(size * size);
-        //_b.resize(size);
+        _b.resize(size);
     }
 
-    friend std::istream& operator>>(std::istream& is, Matrix& mt);
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& mt);
+    friend std::istream &operator>>(std::istream &is, Matrix &mt);
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &mt);
 
-    const int getSize(){
+    const int getSize()
+    {
         return _size;
     }
-    double& operator()(int row, int col)
+    double &operator()(int row, int col)
     {
         return _data[row * _size + col];
     }
- 
+
     double operator()(int row, int col) const
     {
         return _data[row * _size + col];
@@ -38,7 +44,7 @@ public:
     //     }
     //     return s;
     // }
- 
+
     // double operator()(int row) const
     // {
     //     double s = 0;
@@ -47,23 +53,34 @@ public:
     //     }
     //     return s;
     // }
-    void colSwap(int col1, int col2){
-        for(int i = 0; i < _size; i++){
+    void colSwap(int col1, int col2)
+    {
+        for (int i = 0; i < _size; i++)
+        {
             std::swap(_data[i * _size + col1], _data[i * _size + col2]);
         }
     }
-    void rowSwap(int row1, int row2, int submatrix_size){
-        std::swap_ranges(_data.begin() + row1 * _size + _size - submatrix_size, 
+    void rowSwap(int row1, int row2, int submatrix_size)
+    {
+        std::swap_ranges(_data.begin() + row1 * _size + _size - submatrix_size,
                          _data.begin() + row1 * _size + _size,
                          _data.begin() + row2 * _size + _size - submatrix_size);
     }
-    std::pair<int, int> maxElem(int submatrix_size){
+    void bSwap(int row1, int row2)
+    {
+        std::swap(_b[row1], _b[row2]);
+    }
+    std::pair<int, int> maxElem(int submatrix_size)
+    {
         int i_max = 0;
         int j_max = 0;
         double current_max = 0;
-        for(int i = _size - submatrix_size; i < _size; i++){
-            for(int j = _size - submatrix_size; j < _size; j++){
-                if (std::abs(_data[i * _size + j]) > std::abs(current_max)){
+        for (int i = _size - submatrix_size; i < _size; i++)
+        {
+            for (int j = _size - submatrix_size; j < _size; j++)
+            {
+                if (std::abs(_data[i * _size + j]) > std::abs(current_max))
+                {
                     current_max = _data[i * _size + j];
                     i_max = i;
                     j_max = j;
@@ -81,31 +98,32 @@ public:
     // }
 };
 
-std::istream& operator>>(std::istream& is, Matrix& mt)
+std::istream &operator>>(std::istream &is, Matrix &mt)
 {
-    for(int i = 0; i < mt._size * mt._size; i++){
+    for (int i = 0; i < mt._size * mt._size; i++)
+    {
         is >> mt._data[i];
     }
-    // for(int i = 0; i < mt._size; i++){
-    //     for(int j = 0; j < mt._size; j += 2){
-    //         mt._b[i] += mt._data[i * mt._size + j];
-    //     }
-    // }
+    for (int i = 0; i < mt._size; i++)
+    {
+        for (int j = 0; j < mt._size; j += 2)
+        {
+            mt._b[i] += mt._data[i * mt._size + j];
+        }
+    }
     return is;
 }
-std::ostream& operator<<(std::ostream& os, const Matrix& mt)
-{   
-    for(int i = 0; i < mt._size; i++){
-        for(int j = 0; j < mt._size; j++){
+std::ostream &operator<<(std::ostream &os, const Matrix &mt)
+{
+    for (int i = 0; i < mt._size; i++)
+    {
+        for (int j = 0; j < mt._size; j++)
+        {
             os << mt._data[i * mt._size + j] << " ";
         }
-        // os << mt._b[i];
+        os << mt._b[i];
         //os << mt(i);
         os << std::endl;
     }
     return os;
 }
-
-
-
-
